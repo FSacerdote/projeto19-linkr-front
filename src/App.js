5be34e627b7b0002648a0cfd2676010ec9eba275
Nathan4Andrade <nathanandrade20@gmail.com>
2023-08-15 15:34:08 -0300

    feat: LikeButton design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ResetCss from './style/ResetCss';
-import GlobalStyle from './style/GlobalStyle';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ResetCss from "./style/ResetCss";
+import GlobalStyle from "./style/GlobalStyle";
+
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       <ResetCss />
       <GlobalStyle />
       <Routes>
-        <Route path="/" />
+
+        <Route path="/"  />
       </Routes>
     </BrowserRouter>
   );
