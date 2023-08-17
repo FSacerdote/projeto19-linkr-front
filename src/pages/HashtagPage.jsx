cce1a00c9b276af7a53ead98bfe582bf48162fb3
MarcusDantas <marvinsd@gmail.com>
2023-08-17 15:31:32 -0300

    feat: add page with hashtags posts

--- src/pages/HashtagPage.jsx
+++ src/pages/HashtagPage.jsx
@@ -2,18 +2,64 @@ import { styled } from "styled-components";
 import Header from "../components/Header";
 import Post from "../components/Post";
 import TrendingBoard from "../components/TrendingBoard";
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
 import { useParams } from "react-router-dom";
 
-export default function HashtagPage() {
-    const { hashtag } = useParams();
+export default function TimelinePage() {
+  const { hashtag } = useParams();
+  const [message, setMessage] = useState("Loading...");
+
+  const config = {
+    headers: {
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjkyMTYxOTc5LCJleHAiOjE2OTQ3NTM5Nzl9.vPyTUyhgbh2FXzjq4fbbjWXTICseCRA3FkmA2rqknGI`,
+    },
+  };
+  const configRef = useRef(config);
+
+  const [posts, setPosts] = useState(undefined);
+
+  useEffect(() => {
+    setPosts(undefined);
+    axios
+      .get(
+        `${process.env.REACT_APP_API_URL}/hashtags/${hashtag}`,
+        configRef.current
+      )
+      .then((resposta) => {
+        setPosts(resposta.data);
+        console.log(resposta.data);
+      })
+      .catch(() => {
+        setMessage(
+          "An error occured while trying to fetch the posts, please refresh the page"
+        );
+      });
+  }, [hashtag]);
+
+  if (!posts) {
+    return (
+      <Page>
+        <Header></Header>
+        <Timeline>
+          <h1># {hashtag}</h1>
+          <h2>{message}</h2>
+        </Timeline>
+        <TrendingBoard></TrendingBoard>
+      </Page>
+    );
+  }
+
   return (
     <Page>
       <Header></Header>
       <Timeline>
         <h1># {hashtag}</h1>
-        <Post></Post>
-        <Post></Post>
-        <Post></Post>
+        {posts.length === 0 ? (
+          <h2>There are no posts yet</h2>
+        ) : (
+          posts.map((post) => <Post key={post.id} post={post} />)
+        )}
       </Timeline>
       <TrendingBoard></TrendingBoard>
     </Page>
@@ -49,7 +95,13 @@ const Timeline = styled.div`
     line-height: normal;
     margin-bottom: 27px;
   }
-
+  h2 {
+    color: #fff;
+    font-family: "Oswald", sans-serif;
+    font-size: 43px;
+    font-weight: 700;
+    text-align: center;
+  }
   @media (max-width: 1000px) {
     margin-top: 20px;
   }
