5f3b2375aa1754501be1266656343d9689bed15a
MarcusDantas <marvinsd@gmail.com>
2023-08-17 19:48:25 -0300

    fix: conflit error

--- src/App.js
+++ src/App.js
@@ -8,20 +8,14 @@ import SigninPage from "./pages/Login/SigninPage";
 import { useState } from "react";
 import DataContextProvider from "./context/AuthContext";
 
-
-
-import HashtagPage from "./pages/HashtagPage"
+import HashtagPage from "./pages/HashtagPage";
 import { UserPage } from "./pages/UserPage";
-
-
-
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem("token"));
   const contextValue = { token, setToken };
 
   return (
-
     <DataContextProvider.Provider value={contextValue}>
       <BrowserRouter>
         <ResetCss />
@@ -29,26 +23,12 @@ function App() {
         <Routes>
           <Route path="/timeline" element={<TimelinePage />} />
           <Route path="/signup" element={<SignupPage />} />
-          <Route path="/" element={<SigninPage />} />
+          <Route path="/signin" element={<SigninPage />} />
+          <Route path="/user/:id?" element={<UserPage />} />
+          <Route path="/hashtag/:hashtag" element={<HashtagPage />} />
         </Routes>
       </BrowserRouter>
     </DataContextProvider.Provider>
-
-    <BrowserRouter>
-      <ResetCss />
-      <GlobalStyle />
-      <Routes>
-        <Route path="/timeline" element={<TimelinePage />} />
-
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/signin" element={<SigninPage />} />
-
-
-        <Route path="/user/:id?" element={<UserPage />} />
-        <Route path="/hashtag/:hashtag" element={<HashtagPage />} />
-      </Routes>
-    </BrowserRouter>
-
   );
 }
 
--- src/pages/Login/SignupPage.jsx
+++ src/pages/Login/SignupPage.jsx
@@ -1,142 +1,108 @@
-
 import { useNavigate } from "react-router-dom";
 import { Body, Container, Sidebar, StyledLink } from "./FormsStyle";
 import axios from "axios";
 import { useState } from "react";
 
-export default function SignupPage(){
-  const navigate = useNavigate()
-  const [isDisable, setIsDisable] = useState(false)
+export default function SignupPage() {
+  const navigate = useNavigate();
+  const [isDisable, setIsDisable] = useState(false);
   const [formData, setFormData] = useState({
     email: "",
     password: "",
-    username:"",
-    pictureUrl:""
-  })
+    username: "",
+    pictureUrl: "",
+  });
 
-  
-  function handleChange(e){
-    {setFormData({...formData, [e.target.name]:e.target.value})}
+  function handleChange(e) {
+    {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
   }
 
-  function userRegister(e){
-    e.preventDefault()
-    setIsDisable(true)
-    
-    const promise = axios.post(`${process.env.REACT_APP_API_URL}/signup`,formData)
+  function userRegister(e) {
+    e.preventDefault();
+    setIsDisable(true);
+
+    const promise = axios.post(
+      `${process.env.REACT_APP_API_URL}/signup`,
+      formData
+    );
 
     promise.then(() => {
-      alert("Cadastrado com sucesso!")
-      setIsDisable(false)
-      navigate("/")
-    })
-    promise.catch((error)=>{
-      if(error.response && error.response.status === 409){
-        return alert(`${error.response.data} Error ${error.response.status}: ${error.response.statusText}`)
-      } else{
-        alert(error.message)
-
+      alert("Cadastrado com sucesso!");
+      setIsDisable(false);
+      navigate("/");
+    });
+    promise.catch((error) => {
+      if (error.response && error.response.status === 409) {
+        return alert(
+          `${error.response.data} Error ${error.response.status}: ${error.response.statusText}`
+        );
+      } else {
+        alert(error.message);
       }
-      setIsDisable(false)
-    })
+      setIsDisable(false);
+    });
   }
 
-
-import { Body, Container, Sidebar, StyledLink } from "./FormsStyle";
-
-export default function SignupPage(){
-
-  return(
+  return (
     <Body>
       <Sidebar>
         <div>
-        <h1>linkr</h1>
-        <span>save, share and sicover the best links on the web</span>
-
+          <h1>linkr</h1>
+          <span>save, share and sicover the best links on the web</span>
         </div>
       </Sidebar>
 
-    <Container>
-      <label>
+      <Container>
+        <label>
+          <form onSubmit={userRegister}>
+            <input
+              type="email"
+              placeholder="e-mail"
+              value={formData.email}
+              onChange={handleChange}
+              name="email"
+              required
+              disabled={isDisable}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              value={formData.password}
+              onChange={handleChange}
+              name="password"
+              required
+              disabled={isDisable}
+            />
+            <input
+              type="text"
+              placeholder="username"
+              value={formData.username}
+              onChange={handleChange}
+              name="username"
+              required
+              disabled={isDisable}
+            />
+            <input
+              type="url"
+              placeholder="picture url"
+              value={formData.pictureUrl}
+              onChange={handleChange}
+              name="pictureUrl"
+              required
+              disabled={isDisable}
+            />
+            <button type="submit" disabled={isDisable}>
+              Sign Up
+            </button>
+          </form>
+        </label>
 
-      <form onSubmit={userRegister}>
-        <input
-          type="email"
-          placeholder="e-mail"
-          value={formData.email}
-          onChange={handleChange}
-          name="email"
-          required
-          disabled={isDisable}
+        <StyledLink to="/">Switch back to log in</StyledLink>
 
-      <form>
-        <input
-          type="email"
-          placeholder="e-mail"
-          name="email"
-          required
-
-        />
-        <input
-          type="password"
-          placeholder="password"
-
-          value={formData.password}
-          onChange={handleChange}
-          name="password"
-          required
-          disabled={isDisable}
-
-          name="password"
-          required
-
-        />
-        <input
-          type="text"
-          placeholder="username"
-
-          value={formData.username}
-          onChange={handleChange}
-          name="username"
-          required
-          disabled={isDisable}
-
-          name="username"
-          required
-
-        />
-        <input
-          type="url"
-          placeholder="picture url"
-          value={formData.pictureUrl}
-          onChange={handleChange}
-          name="pictureUrl"
-          required
-          disabled={isDisable}
-        />
-      <button type="submit" disabled={isDisable}>Sign Up</button>
-
-          name="pictureUrl"
-          required
-        />
-      <button>Sign Up</button>
-
-      </form>
-
-      </label>
-
-
-      <StyledLink to="/">Switch back to log in</StyledLink>
-
-      <StyledLink>Switch back to log in</StyledLink>
-
-    </Container>
+        <StyledLink>Switch back to log in</StyledLink>
+      </Container>
     </Body>
-      
-  )
-
-
+  );
 }
-
-}
-
