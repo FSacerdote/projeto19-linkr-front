35b8cc835a305f710ba23ac44603cd7611565a07
MarcusDantas <marvinsd@gmail.com>
2023-08-22 20:25:20 -0300

    fix: infinite scroll

---
+++ src/components/LoadingMorePosts.jsx
@@ -0,0 +1,24 @@
+import { styled } from "styled-components";
+import { TailSpin } from "react-loader-spinner";
+
+export default function LoadingMorePosts() {
+  return (
+    <Loading>
+      <TailSpin width="40" radius="0" color="#6d6d6d"></TailSpin>
+      <p>Loading more posts...</p>
+    </Loading>
+  );
+}
+
+const Loading = styled.div`
+  margin: 60px auto 200px auto;
+  width: 226px;
+  height: 78px;
+  font-family: "Lato", sans-serif;
+  color: #6d6d6d;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 24px;
+`;
--- src/pages/HashtagPage.jsx
+++ src/pages/HashtagPage.jsx
@@ -6,23 +6,24 @@ import { useEffect, useRef, useState, useContext } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import DataContextProvider from "../context/AuthContext";
-import InfiniteScroll from "react-infinite-scroller";
 import { useInterval } from "usehooks-ts";
+import LoadingMorePosts from "../components/LoadingMorePosts";
 
 export default function TimelinePage() {
   const { hashtag } = useParams();
   const [message, setMessage] = useState("Loading...");
   const [page, setPage] = useState(0);
   const [offset, setOffset] = useState(0);
-  const [morePages, setMorePages] = useState(true);
+  const [morePages, setMorePages] = useState(false);
   const [newPosts, setNewPosts] = useState([]);
   const [firstPostId, setFirstPostId] = useState(0);
   const [alertNewPosts, setAlertNewPosts] = useState(false);
+  const [pageLoading, setPageLoading] = useState(true);
 
   const { config } = useContext(DataContextProvider);
   const configRef = useRef(config);
 
-  const [posts, setPosts] = useState(undefined);
+  const [posts, setPosts] = useState([]);
 
   function includeNewPosts() {
     setAlertNewPosts(false);
@@ -30,53 +31,86 @@ export default function TimelinePage() {
   }
 
   useInterval(() => {
-    axios
-      .get(
-        `${process.env.REACT_APP_API_URL}/hashtag/${hashtag}/new/${firstPostId}`,
-        configRef.current
-      )
-      .then((resposta) => {
-        if (resposta.data.length !== 0) {
-          setOffset((prevOffset) => prevOffset + resposta.data.length);
-          setNewPosts((prevPosts) => [...prevPosts, ...resposta.data]);
-          setFirstPostId(resposta.data[0].id);
-          setAlertNewPosts(true);
-        }
-      })
-      .catch(() => {
-        setMessage(
-          "An error occured while trying to fetch the posts, please refresh the page"
-        );
-      });
+    //axios
+    //  .get(
+    //    `${process.env.REACT_APP_API_URL}/hashtag/${hashtag}/new/${firstPostId}`,
+    //    configRef.current
+    //  )
+    //  .then((resposta) => {
+    //    if (resposta.data.length !== 0) {
+    //      setOffset((prevOffset) => prevOffset + resposta.data.length);
+    //      setNewPosts((prevPosts) => [...prevPosts, ...resposta.data]);
+    //      setFirstPostId(resposta.data[0].id);
+    //      setAlertNewPosts(true);
+    //    }
+    //  })
+    //  .catch(() => {
+    //    setMessage(
+    //      "An error occured while trying to fetch the posts, please refresh the page"
+    //    );
+    //  });
   }, 15000);
 
-  useEffect(() => {
-    if (page === 0) {
-      setPosts(undefined);
-    }
+  function getPosts(currentPage) {
+    setPageLoading(true);
+    const currentOffset = currentPage === 0 ? 0 : offset;
     axios
       .get(
-        `${process.env.REACT_APP_API_URL}/hashtag/${hashtag}?limit=10&offset=${offset}`,
+        `${process.env.REACT_APP_API_URL}/hashtag/${hashtag}?limit=10&offset=${currentOffset}`,
         configRef.current
       )
       .then((resposta) => {
+        if (currentPage === 0 && resposta.data.length === 0) {
+          setPageLoading(false);
+          return setPosts(["empty"]);
+        }
         setOffset((prevOffset) => prevOffset + 10);
         setPosts((prevPosts) => [...prevPosts, ...resposta.data]);
+
         if (resposta.data.length < 10) {
           setMorePages(false);
+        } else {
+          setMorePages(true);
         }
-        if (page === 0) {
-          setFirstPostId(resposta.data[0].id)
-        }
+        setPageLoading(false);
       })
-      .catch(() => {
+      .catch((error) => {
+        console.log(error);
         setMessage(
           "An error occured while trying to fetch the posts, please refresh the page"
         );
       });
-  }, [hashtag, page]);
+  }
 
-  if (!posts) {
+  function handleScroll() {
+    const windowHeight = window.innerHeight;
+    const fullPageHeight = document.documentElement.scrollHeight;
+    const scrolledHeight = window.scrollY;
+
+    if (scrolledHeight + windowHeight >= fullPageHeight) {
+      //getPosts(page+1);
+      //setPage(prevPage => prevPage + 1);
+      console.log("oi");
+    }
+  }
+
+  useEffect(() => {
+    setNewPosts([]);
+    setPageLoading(true);
+    setPosts([]);
+    setMorePages(false);
+    setFirstPostId(0);
+    setOffset(0);
+    setPage(0);
+    getPosts(0);
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [hashtag]);
+
+  if (posts.length === 0) {
     return (
       <Page>
         <Header></Header>
@@ -94,19 +128,15 @@ export default function TimelinePage() {
       <Header></Header>
       <Timeline>
         <h1 data-test="hashtag-title"># {hashtag}</h1>
-        {alertNewPosts && <h2 onClick={includeNewPosts}>NewPosts {newPosts.length}</h2>}
-        {posts.length === 0 ? (
-          <h2>There are no posts yet</h2>
-        ) : ( 
-          <InfiniteScroll
-            pageStart={1}
-            loadMore={() => setPage((prevPage) => prevPage + 1)}
-            hasMore={morePages}
-            loader={<h2>Loading...</h2>}
-          >
-            {posts.map((post) => <Post key={post.id} post={post} />)}
-          </InfiniteScroll>
+        {alertNewPosts && (
+          <h2 onClick={includeNewPosts}>NewPosts {newPosts.length}</h2>
         )}
+        {posts[0] === "empty" ? (
+          <h2>There are no posts yet</h2>
+        ) : (
+          posts.map((post) => <Post key={post.id} post={post} />)
+        )}
+        {morePages && <LoadingMorePosts />}
       </Timeline>
       <TrendingBoard></TrendingBoard>
     </Page>
